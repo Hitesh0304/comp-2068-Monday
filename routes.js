eb4e4d6bd2a774f65c9acf71cd24319bf088419d
hitesh0304 <200378502@student.georgianc.on.ca>
2019-06-03 15:50:08 -0400

    adding part-02

---
+++ routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+//Import our page routes
+const pageRoutes = require('./routes/pages');
+const blogRoutes = require('./routes/blogs');
+
+//Register our page routes
+app.use('/', pageRoutes);
+app.use('/blogs', blogRoutes);
+
+// Exporting the changes
+module.exports = app;
